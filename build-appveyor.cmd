2daffcff99e92cd84ffc5aee561137dc5f57af46
Jamie Pollock <jamie.pollock@connect-group.com>
2016-06-09 23:57:17 +0100

    Corrected nuget restore command call (copypasta fail)

--- build-appveyor.cmd
+++ build-appveyor.cmd
@@ -3,5 +3,5 @@ ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
 ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 
-CALL build\tools\NuGet.exe restore src\Our.Umbraco.AzureLogger.sln
+CALL build\tools\NuGet.exe restore src\Our.Umbraco.MigrationsViewer.sln
 CALL "%programfiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" build\Our.Umbraco.MigrationsViewer.proj
